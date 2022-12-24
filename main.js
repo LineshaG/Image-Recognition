58d3426250fcc1dc3bc432fde7e43213f1cf1fd0
LineshaG <88967931+LineshaG@users.noreply.github.com>
2022-12-24 19:42:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-https://teachablemachine.withgoogle.com/models/TtYxybVLc/
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("Camera");
+Webcam.attach('#camera');
+
+ function Capture(){
+    Webcam.snap(function(data_uri) 
+    {
+    document.getElementById("Result").innerHTML = "<img id='result' src=" + data_uri + ">";
+    });
+ }
+
+ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/TtYxybVLc/model.json", modelLoaded);
+ 
+ function modelLoaded()
+ {
+    console.log("modelLoaded");
+ }
